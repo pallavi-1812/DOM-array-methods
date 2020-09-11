75658434645993e1cdaf8ca5c45dd35489206e5e
pallavi1812 <upallavi1812@gmail.com>
2020-09-11 15:24:00 +0530

    second commit

---
+++ script.js
@@ -0,0 +1,5 @@
+const add_user = document.getElementById('add_user');
+const double_money = document.getElementById('double_money');
+const show_mill = document.getElementById('show_mill');
+const sort_richest = document.getElementById('sort_richest');
+const total_wealth = document.getElementById('total_wealth');
